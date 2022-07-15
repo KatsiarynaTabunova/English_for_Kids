70cda0dc74c4ec85839139ed05108f8c3867dd2c
tabunova.katsiaryna <katepilipionak18.90@gmail.com>
2022-07-15 14:27:24 +0300

    deleted footer

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,6 @@ import '../styles/app';
 import {parseCurrentURL} from './helpers/utils.js';
 
 import Header from './views/partials/header.js';
-import Footer from './views/partials/footer.js';
 
 import AddAndList from './views/pages/tasks/add-list.js';
 import Info from './views/pages/tasks/info.js';
@@ -14,7 +13,6 @@ import Error404 from './views/pages/error404.js';
 import Games from './views/pages/games/games';
 import Game from './views/pages/games/game';
 import FirstLevel from './views/pages/levels/level-1';
-
 
 const Routes = {
     '/': About,
@@ -43,13 +41,6 @@ function router() {
         page.afterRender();
     })();
 }
-
-(async() => {
-    const footerContainer = document.getElementsByClassName('footer-container')[0],
-        footer = new Footer();
-
-    footerContainer.innerHTML = await footer.render();
-})();
 
 module.hot ? module.hot.accept(router()) : (window.onload = router);
 window.onhashchange = router;
