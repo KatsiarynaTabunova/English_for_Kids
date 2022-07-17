35bb7b00d48d2b3bfe5265a528ff3f31b9362f1c
tabunova.katsiaryna <katepilipionak18.90@gmail.com>
2022-07-17 20:10:05 +0300

    added button next with functionallity

--- client/scripts/views/pages/levels/first-level.js
+++ client/scripts/views/pages/levels/first-level.js
@@ -14,63 +14,58 @@ class FirstLevel extends Component {
         const blockImages = document.getElementsByClassName('container-level1__images')[0];
         const blockSound = document.getElementsByClassName('sound')[0];
         const buttonNextSound = document.getElementsByClassName('container-level1__button-switcher')[0];
-        const gameItems = GameModel.getRandomGameItems();
-        const correctGameItem = gameItems[0];
+        let gameItems;
+        let correctGameItem;
         let isGameActive = true;
-
+        let allImages;
 
         showRandomImages();
-        const allImages = Array.from(document.getElementsByClassName('game-item__img'));
 
         function showRandomImages() {
-            // blockImages.innerHTML = '';
+            gameItems = GameModel.getRandomGameItems();
+            correctGameItem = gameItems[0];
             blockImages.innerHTML = RandomImagesTemplate({gameItems});
 
+            allImages = Array.from(document.getElementsByClassName('game-item__img'));
+
+            blockImages.addEventListener('click', (event) => {
+                if (!isGameActive) {
+                    return;
+                }
+                if (event.target.classList.contains('game-item__img')) {
+                    const selectedImage = event.target;
+
+                    if (selectedImage.getAttribute('src') === correctGameItem.imageUrl) {
+                        selectedImage.classList.add('green');
+                    } else {
+                        selectedImage.classList.add('red');
+                        console.log(findCorrectGameItemImage());
+                        findCorrectGameItemImage().classList.add('green');
+                    }
+
+                    allImages.forEach(item => {
+                        if (!item.classList.contains('green')) {
+                            item.classList.add('disable');
+                        }
+                    });
+
+                    isGameActive = false;
+                }
+            });
         }
 
         blockSound.addEventListener('click', () => {
-
             const audio = new Audio(correctGameItem.sound);
             audio.play();
         });
 
-        blockImages.addEventListener('click', (event) => {
-            if (!isGameActive) {
-                return;
-            }
-            if (event.target.classList.contains('game-item__img')) {
-                const selectedImage = event.target;
-
-                if (selectedImage.getAttribute('src') === correctGameItem.imageUrl) {
-                    selectedImage.classList.add('green');
-                } else {
-                    selectedImage.classList.add('red');
-                    findCorrectGameItemImage().classList.add('green');
-                }
-
-                allImages.forEach(item => {
-                    if (!item.classList.contains('green')) {
-                        item.classList.add('disable');
-                    }
-                });
-
-                isGameActive = false;
-            }
-        });
         buttonNextSound.addEventListener('click', () => {
             isGameActive = true;
-            clearSelectedImages();
-            allImages.forEach(item => item.classList.remove('disable'));
-            // showRandomImages();
-
+            showRandomImages();
         });
 
         function findCorrectGameItemImage() {
             return allImages.find(item => item.getAttribute('src') === correctGameItem.imageUrl);
-        }
-
-        function clearSelectedImages() {
-            allImages.forEach(item => item.classList.remove('red', 'green'));
         }
     }
 }
