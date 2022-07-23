eb0e2e45e9d115aec357077e04e5ebc452fd4b0d
tabunova.katsiaryna <katepilipionak18.90@gmail.com>
2022-07-23 10:49:20 +0300

    add drag and drop(continue)

--- client/scripts/views/pages/levels/third-level-page.js
+++ client/scripts/views/pages/levels/third-level-page.js
@@ -4,6 +4,7 @@ import ThirdLevelTemplate from '../../../../templates/pages/levels/third-level-t
 import GameModel from '../../../models/game-model';
 import {parseCurrentURL} from '../../../helpers/utils';
 
+
 class ThirdLevelPage extends Component {
     async render() {
         const urlParts = parseCurrentURL();
@@ -20,36 +21,45 @@ class ThirdLevelPage extends Component {
     }
 
     afterRender() {
-        // const mainContainer = document.getElementsByClassName('third-level__container')[0];
-        // const fieldsBlock = document.getElementsByClassName('third-level__field-list')[0];
-        // const currentImage = document.getElementsByClassName('third-level__list-options')[0];
-        // const DragManager = new function() {
 
         document.onmousedown = function(event) {
-            const draggableImage = event.target;
-
-            if (draggableImage.classList.contains('third-level__list-options-image')) {
-                draggableImage.style.position = 'absolute';
-                draggableImage.style.border = '1px dashed black';
+            // let initX;
+            // let initY;
+            if (event.target.classList.contains('third-level__list-options-image')) {
+                let draggableImage = event.target;
+                draggableImage.classList.add('absolute');
                 moveImage(event, draggableImage);
-                // document.body.appendChild(draggableImage);
+
+                // initX = draggableImage.style.left;
+                // initY = draggableImage.style.top;
 
                 draggableImage.style.zIndex = 1000;
 
+
                 document.onmousemove = function(event) {
                     moveImage(event, draggableImage);
+                    console.log('move');
                 };
+
                 document.onmouseup = function() {
+                    console.log('up');
                     document.onmousemove = null;
                     draggableImage.onmouseup = null;
-                    findInputField(event, draggableImage);
-                    draggableImage.style.border = 'none';
+                    const targetParentField = findTargetField(event, draggableImage);
+                    // if (targetParentField === null) {
+                    //     console.log('go back');
+                    // } else {
+                    console.log('goood');
+                    const targetField = targetParentField.getElementsByClassName('third-level__field-image')[0];
+
+                    targetField.appendChild(draggableImage);
+                    draggableImage.classList.remove('absolute');
+                    // }
                 };
                 draggableImage.ondragstart = function() {
                     return false;
                 };
             }
-
         };
 
         function moveImage(event, image) {
@@ -57,20 +67,24 @@ class ThirdLevelPage extends Component {
             image.style.top = event.pageY - image.offsetHeight / 2 + 'px';
         }
 
-        // function finishDrag(event, image) {
-        //     findInputField(event, image);
-        // }
+        function findTargetField(event, image) {
+            const imageRect = image.getBoundingClientRect();
+            const targetFields = document.getElementsByClassName('third-level__field');
 
-        function findInputField(event, image) {
-            // image.hidden = true;
-            // const elem = document.elementFromPoint(event.clientX, event.clientY);
-            // image.hidden = false;
-            // if (elem == null) {
-            //     return null;
-            // }
-            console.log(image);
-            console.log(image.closest('third-level__field'));
-            return image.closest('.third-level__field');
+            for (const targetField of targetFields) {
+                if (areRectanglesIntercepted(imageRect, targetField.getBoundingClientRect())) {
+
+                    return targetField;
+                }
+            }
+            return null;
+        }
+
+        function areRectanglesIntercepted(rect1, rect2) {
+            return rect1.bottom > rect2.top
+                && rect1.right > rect2.left
+                && rect1.top < rect2.bottom
+                && rect1.left < rect2.right;
         }
 
     }
